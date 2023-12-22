3453b670fd5a3a23e8cd704091d8628953dd0eb3
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-22 08:52:08 -0700

    feat: support redirects in different environments

--- src/routes/auth/login/+page.server.js
+++ src/routes/auth/login/+page.server.js
@@ -1,11 +1,11 @@
 import { fail, redirect } from '@sveltejs/kit';
 
 export const actions = {
-	default: async ({ request, locals: { supabase } }) => {
+	default: async ({ request, url: { origin }, locals: { supabase } }) => {
 		const formData = await request.formData();
 		const email = formData.get('email');
 
-		const { error } = await supabase.auth.signInWithOtp({ email });
+		const { error } = await supabase.auth.signInWithOtp({ email, redirectTo: origin });
 		if (error) {
 			return fail(500, { message: 'Server error. Try again later.', success: false, email });
 		}
