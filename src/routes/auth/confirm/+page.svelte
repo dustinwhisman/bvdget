cb2f1ab95f0b84e7c01db04526d4bf2924d7f54f
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-23 09:36:01 -0700

    feat: use one-time code for login (#3)

---
+++ src/routes/auth/confirm/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+</script>
+
+<h1>Confirm Login</h1>
+<p>Check your email for a one-time code that you can use to confirm your login.</p>
+
+<form method="POST">
+	<input type="hidden" name="email" value={data.email} />
+	<label for="token">One-time code</label>
+	<input id="token" type="text" name="token" inputmode="numeric" autocomplete="one-time-code" />
+	<button type="submit">Finish logging in</button>
+</form>
