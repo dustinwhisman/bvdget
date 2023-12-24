7fe0033d16d9c4fa21c6917b2df666244137f9ce
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-24 08:30:41 -0700

    feat: extract reusable UI elements for overview pages

--- src/routes/overview/+page.svelte
+++ src/routes/overview/+page.svelte
@@ -1,49 +1,7 @@
 <script>
+	import MonthlyOverview from '../../components/MonthlyOverview.svelte';
+
 	export let data;
 </script>
 
-<h1>Overview</h1>
-
-<h2>Expenses</h2>
-{#if data.expenses.length}
-	<ul>
-		{#each data.expenses as expense}
-			<li>{expense.description}: {expense.amount}</li>
-		{/each}
-	</ul>
-{:else}
-	<p>No expenses found.</p>
-{/if}
-
-<h2>Income</h2>
-{#if data.income.length}
-	<ul>
-		{#each data.income as incomeEntry}
-			<li>{incomeEntry.description}: {incomeEntry.amount}</li>
-		{/each}
-	</ul>
-{:else}
-	<p>No income found.</p>
-{/if}
-
-<h2>Savings</h2>
-{#if data.savings.length}
-	<ul>
-		{#each data.savings as savingsEntry}
-			<li>{savingsEntry.description}: {savingsEntry.amount}</li>
-		{/each}
-	</ul>
-{:else}
-	<p>No savings found.</p>
-{/if}
-
-<h2>Debt</h2>
-{#if data.debt.length}
-	<ul>
-		{#each data.debt as debtEntry}
-			<li>{debtEntry.description}: {debtEntry.amount}</li>
-		{/each}
-	</ul>
-{:else}
-	<p>No debt found.</p>
-{/if}
+<MonthlyOverview {data} />
