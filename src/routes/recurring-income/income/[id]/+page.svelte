7dc5fc5840613bc857fe56d147ec3c867dd47816
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-26 14:55:17 -0700

    feat: support editing and deleting recurring income

---
+++ src/routes/recurring-income/income/[id]/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import AmountInput from '../../../../components/inputs/AmountInput.svelte';
+	import CategoryInput from '../../../../components/inputs/CategoryInput.svelte';
+	import DescriptionInput from '../../../../components/inputs/DescriptionInput.svelte';
+	import FrequencyInput from '../../../../components/inputs/FrequencyInput.svelte';
+	import IsActiveInput from '../../../../components/inputs/IsActiveInput.svelte';
+
+	export let data;
+</script>
+
+<h1>Edit Recurring Income</h1>
+
+<form method="POST" action="?/edit">
+	<input type="hidden" name="id" value={data.income.id} />
+	<CategoryInput categories={data.categories} selectedCategory={data.income.category} />
+	<DescriptionInput description={data.income.description} />
+	<AmountInput amount={data.income.amount} />
+	<FrequencyInput
+		selectedFrequency={data.income.frequency}
+		daysOfMonth={data.income.days_of_month}
+		date={data.income.date}
+	/>
+	<IsActiveInput isActive={data.income.active} />
+	<div>
+		<button type="submit">Save Recurring Income</button>
+		<button type="submit" formaction="?/delete">Delete Recurring Income</button>
+	</div>
+</form>
