2ee9e816833d284c7cd19d7ba9e1eb26ed322acc
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-26 14:15:25 -0700

    feat: create a component for the active checkbox

---
+++ src/components/inputs/IsActiveInput.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let isActive = false;
+</script>
+
+<div>
+	<input id="is-active" type="checkbox" name="active" checked={isActive} />
+	<label for="is-active">This is currently active</label>
+</div>
