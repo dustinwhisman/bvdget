8d60297904131a2d50b882618dd26449fd6ab113
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-26 13:31:48 -0700

    feat: show all the recurring expenses for a user

---
+++ src/routes/recurring-expenses/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let data;
+</script>
+
+<h1>Recurring Expenses</h1>
+
+{#if data.recurringExpenses.length}
+	<ul>
+		{#each data.recurringExpenses as expense}
+			<li>
+				{expense.description}: {expense.amount}
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No recurring expenses found.</p>
+{/if}
