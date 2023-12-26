b7f4628673027a3459347c2e7d9c38003b1dd486
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-26 10:16:35 -0700

    feat: save expense to DB

--- src/routes/expense/+page.server.js
+++ src/routes/expense/+page.server.js
@@ -1,4 +1,5 @@
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
+import { formatAmount, formatDate } from '$lib/format-inputs.js';
 
 export const load = async ({ locals: { supabase } }) => {
 	try {
@@ -13,4 +14,35 @@ export const load = async ({ locals: { supabase } }) => {
 	} catch (error) {
 		return fail(500, { message: 'Server error. Try again later.', success: false });
 	}
+};
+
+export const actions = {
+	default: async ({ request, locals: { supabase } }) => {
+		const formData = await request.formData();
+		const amount = formatAmount(formData.get('amount'));
+
+		const selectedCategory = formData.get('category');
+		const newCategory = formData.get('new-category');
+		const category = newCategory ?? selectedCategory;
+
+		const description = formData.get('description') || category;
+
+		const year = formData.get('year');
+		const month = formData.get('month');
+		const day = formData.get('day');
+		const date = formatDate(year, month, day);
+
+		const {
+			data: { user },
+		} = await supabase.auth.getUser();
+		const { error } = await supabase
+			.from('expenses')
+			.insert({ user_id: user.id, date, category, description, amount });
+
+		if (error) {
+			return fail(500, { message: 'Server error. Try again later.', success: false });
+		}
+
+		throw redirect(303, '/overview');
+	},
 };
