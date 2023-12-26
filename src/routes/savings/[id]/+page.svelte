e72bb2d013ee9ad3aac829a0ad0e833da8bbec69
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-26 13:01:48 -0700

    feat: support deleting savings

--- src/routes/savings/[id]/+page.svelte
+++ src/routes/savings/[id]/+page.svelte
@@ -9,7 +9,7 @@
 
 <h1>Edit Savings</h1>
 
-<form method="POST">
+<form method="POST" action="?/edit">
 	<input type="hidden" name="id" value={data.savings.id} />
 	<DateInputs showDate={false} date={data.savings.date} />
 	<CategoryInput categories={data.categories} selectedCategory={data.savings.category} />
@@ -17,5 +17,6 @@
 	<AmountInput label="Balance" amount={data.savings.amount} />
 	<div>
 		<button type="submit">Save Savings</button>
+		<button type="submit" formaction="?/delete">Delete Savings</button>
 	</div>
 </form>
