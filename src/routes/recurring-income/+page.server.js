60aa1434a928cc8a6cf330198d8c350badedd98b
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-27 08:29:31 -0700

    feat: gate recurring income pages from unauthed users

--- src/routes/recurring-income/+page.server.js
+++ src/routes/recurring-income/+page.server.js
@@ -1,4 +1,8 @@
+import { gateDynamicPage } from '$lib/gate-dynamic-page.js';
+
 export const load = async ({ locals: { supabase } }) => {
+	await gateDynamicPage(supabase);
+
 	const { data: recurringIncome } = await supabase
 		.from('recurring_income')
 		.select('id,date,description,amount,frequency,active')
