316af1dc62ad9e66529c1e8e955ca361329083e6
Dustin Whisman <whisman.dustin@gmail.com>
2023-12-28 09:24:34 -0700

    feat: set up build scripts and linting for styles using SCSS (#19)
    * feat: replace favicon with emoji (for now)
    
    * chore: install sass and stylelint dependencies
    
    * feat: set up CSS reset and initial SCSS structure
    
    * chore: set up linting for SCSS
    
    * chore: set up build/dev sass scripts
    
    * chore: reload page on *.scss file changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+const ReloadOnSCSSChange = () => ({
+	name: 'styles-hmr',
+	enforce: 'post',
+	handleHotUpdate({ file, server }) {
+		if (file.endsWith('.scss')) {
+			server.ws.send({
+				type: 'full-reload',
+				path: '*',
+			});
+		}
+	},
+});
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), ReloadOnSCSSChange()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
