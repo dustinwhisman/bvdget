8ec074c0fc3dfd6caa4b57aae3e467de76052e2a
Dustin Whisman <whisman.dustin@gmail.com>
2024-01-05 15:43:27 -0700

    feat: style the authentication forms (#23)
    * feat: style the form to request a code
    
    * feat: style login confirmation form
    
    * style: fix formatting issues

--- src/routes/auth/confirm/+page.svelte
+++ src/routes/auth/confirm/+page.svelte
@@ -16,11 +16,18 @@
 	{:else}
 		<p>Check your email for a one-time code that you can use to confirm your login.</p>
 
-		<form method="POST" aria-label="Finish logging in">
+		<form method="POST" class="cmp-form" aria-label="Finish logging in">
 			<input type="hidden" name="email" value={data.email} />
-			<label for="token">One-time code</label>
-			<input id="token" type="text" name="token" inputmode="numeric" autocomplete="one-time-code" />
-			<button type="submit">Finish logging in</button>
+			<label for="token" class="cmp-form__label">One-time code</label>
+			<input
+				id="token"
+				type="text"
+				name="token"
+				class="cmp-form__input"
+				inputmode="numeric"
+				autocomplete="one-time-code"
+			/>
+			<button type="submit" class="cmp-form__button">Finish logging in</button>
 		</form>
 	{/if}
 </div>
