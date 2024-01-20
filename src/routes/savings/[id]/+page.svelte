99def9c4a3ed032cd8655e24b7b09cdf54e81830
Dustin Whisman <whisman.dustin@gmail.com>
2024-01-20 09:21:27 -0700

    feat: style savings forms

--- src/routes/savings/[id]/+page.svelte
+++ src/routes/savings/[id]/+page.svelte
@@ -9,14 +9,16 @@
 
 <h1>Edit Savings</h1>
 
-<form method="POST" action="?/edit" aria-label="Edit savings">
+<form method="POST" action="?/edit" class="cmp-form" aria-label="Edit savings">
 	<input type="hidden" name="id" value={data.savings.id} />
 	<DateInputs showDate={false} date={data.savings.date} />
 	<CategoryInput categories={data.categories} selectedCategory={data.savings.category} />
 	<DescriptionInput description={data.savings.description} />
 	<AmountInput label="Balance" amount={data.savings.amount} />
-	<div>
-		<button type="submit">Save Savings</button>
-		<button type="submit" formaction="?/delete">Delete Savings</button>
+	<div class="cmp-form__button-group">
+		<button type="submit" class="cmp-form__button">Save Savings</button>
+		<button type="submit" formaction="?/delete" class="cmp-form__button cmp-form__button--danger"
+			>Delete Savings</button
+		>
 	</div>
 </form>
