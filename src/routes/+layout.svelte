d3d67a83b59abd7afa477c09b1265869bfd1e00e
dustinwhisman <whisman.dustin@gmail.com>
2024-05-08 15:06:14 -0600

    refactor: move container wrapper div inside landmarks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,17 +30,21 @@
 	/>
 </svelte:head>
 <header>
-	<a href="#main" class="util-visually-hidden">Skip to main content</a>
-	<nav class="cmp-navigation" aria-label="Primary navigation">
-		{#if session != null}
-			<a href="/overview" class="cmp-navigation__link">Overview</a>
-			<a href="/auth/sign-out" class="cmp-navigation__link">Sign out</a>
-		{:else}
-			<a href="/" class="cmp-navigation__link">Home</a>
-			<a href="/auth/login" class="cmp-navigation__link">Log in/sign up</a>
-		{/if}
-	</nav>
+	<div class="obj-container">
+		<a href="#main" class="util-visually-hidden">Skip to main content</a>
+		<nav class="cmp-navigation" aria-label="Primary navigation">
+			{#if session != null}
+				<a href="/overview" class="cmp-navigation__link">Overview</a>
+				<a href="/auth/sign-out" class="cmp-navigation__link">Sign out</a>
+			{:else}
+				<a href="/" class="cmp-navigation__link">Home</a>
+				<a href="/auth/login" class="cmp-navigation__link">Log in/sign up</a>
+			{/if}
+		</nav>
+	</div>
 </header>
 <main id="main" tabindex="-1">
-	<slot />
+	<div class="obj-container">
+		<slot />
+	</div>
 </main>
