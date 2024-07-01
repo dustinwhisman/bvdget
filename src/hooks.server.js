e7c5dc6bc604eff1be523bca1173b69aa1ed7524
Dustin Whisman <whisman.dustin@gmail.com>
2024-07-01 11:11:37 -0600

    feat: update cookie usage to match new practices

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,12 +4,11 @@ import { createServerClient } from '@supabase/ssr';
 export const handle = async ({ event, resolve }) => {
 	event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, {
 		cookies: {
-			get: (key) => event.cookies.get(key),
-			set: (key, value, options) => {
-				event.cookies.set(key, value, { ...options, path: '/' });
-			},
-			remove: (key, options) => {
-				event.cookies.delete(key, { ...options, path: '/' });
+			getAll: () => event.cookies.getAll(),
+			setAll: (cookiesToSet) => {
+				cookiesToSet.forEach(({ name, value, options }) => {
+					event.cookies.set(name, value, { ...options, path: '/' });
+				});
 			},
 		},
 	});
